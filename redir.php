b93fcce6c02640ee595e2d11095314305a08d5d4
Sabri <MrSherlockHolmes123@gmail.com>
2016-06-25 14:16:51 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,5 +1,1 @@
-You wish to visit the link: <?php echo htmlspecialchars($_GET["u"]); ?>
-
-Are you sure?
-
-<a href="/">No</a> <a href=<?php echo '"'.htmlspecialchars($_GET["u"]).'"'; ?>>Yes</a>
+<?php echo htmlspecialchars($_GET["u"]); ?>
